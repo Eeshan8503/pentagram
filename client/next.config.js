d6efa35fd2ecd99f3ca0d57f68643e89d3a519ac
Eeshan Mattey <71863350+Eeshan8503@users.noreply.github.com>
2023-07-18 23:50:48 +0530

    Update next.config.js

--- client/next.config.js
+++ client/next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    exclude: ["/public/assets/P4.jpg"],
+  },
 }
+
 // const withImages = require('next-images')
 // module.exports = withImages()
 module.exports = nextConfig
